56ad7f3aa00523d70c21d49aff43bf65282d0742
zqmillet <zqmillet@qq.com>
2021-07-30 23:52:09 +0800

    update

--- examples/introductions/second/show_version.py
+++ examples/introductions/second/show_version.py
@@ -3,3 +3,10 @@ import platform
 
 print('Python', sys.version)
 print('OS is', platform.platform())
+
+import subprocess
+process = subprocess.Popen(['dot', '-V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+output, _ = process.communicate()
+
+print(output.decode('utf8'))
+
